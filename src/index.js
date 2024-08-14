7253affe33f1fe314c74901bb2bcc4c6cd886bb4
Yashraj Pravin Desale <yashrajdesale1@gmail.com>
2024-08-14 13:57:52 +0530

    Added User Schema

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const ServerConfig = require('./config/serverConfig')
 const connectDB = require('./config/dbConfig')
-
+const User = require('./schema/UserSchema')
 
 const app = express()
 
 // If request is in JSON, text, urlencoded it correctly reads by Express Server
+// deserialiser
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 app.use(bodyParser.urlencoded());
@@ -19,5 +20,16 @@ app.post('/ping', (req, res) => {
 app.listen(ServerConfig.PORT, async () => {
   await connectDB();
   console.log(`Example app listening on port ${ServerConfig.PORT}...`)
+
+  // For checking
+  // const newUser = await User.create({
+  //   email:'yashr@gmail.com',
+  //   password: '12348677',
+  //   firstName: 'yashraj',
+  //   lastName:'dote',
+  //   mobileNumber:'9768556744'
+  // });
+  // console.log(newUser);
+
 })
 
