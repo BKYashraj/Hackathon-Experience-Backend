5f619564e2f5e72ffae40724fcb9badf98243b7f
Yashraj Pravin Desale <yashrajdesale1@gmail.com>
2024-08-20 01:09:19 +0530

    updated

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,8 @@
 const { JWT_SECRET } = require("../config/serverConfig");
 const User = require("../schema/userSchema");
 const { loginUser } = require("../services/authService");
-const bcryptjs = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 // import { bcryptjs } from 'bcryptjs';
 
 async function login(req, res) {
@@ -12,21 +12,21 @@ async function login(req, res) {
     const response = await loginUser(loginPayload);
 
     // After User login, we send JWT token in the form of cookie to user, so next time when he use any functionality he send this cookie to server then server gives direct access to the user for that functionality.
-    console.log("Response token is ",response.token);
+    console.log("Response token is ", response.token);
     res.cookie("authToken", response.token, {
       httpOnly: true,
       secure: true,
-      sameSite: 'none',
-      maxAge: 7 * 24 * 60 * 60 * 1000, 
-  });
+      sameSite: "none",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
 
     return res.status(200).json({
       success: true,
       message: "Successfully logged in",
-      data: {
-        userData: response.userData,
-        userRole: response.userRole
-      },
+
+      userData: response.userData,
+      userRole: response.userRole,
+
       error: {},
     });
   } catch (error) {
@@ -45,8 +45,8 @@ async function logout(req, res) {
   res.cookie("authToken", "", {
     httpOnly: true,
     secure: true,
-    sameSite: 'none',
-    maxAge:  7 * 24 * 60 * 60 * 1000,
+    sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
   return res.status(200).json({
@@ -60,14 +60,18 @@ async function google(req, res, next) {
   try {
     const user = await User.findOne({ email: req.body.email.toLowerCase() });
     if (user) {
+      if (user.avatar !== req.body.photo) {
+        user.avatar = req.body.photo;
+        await user.save(); // Save the updated user
+      }
       const token = jwt.sign({ id: user._id }, JWT_SECRET);
       const { password: pass, ...rest } = user._doc;
       res
-        .cookie('authToken', token, {
+        .cookie("authToken", token, {
           httpOnly: true,
           secure: true,
-          sameSite: 'none',
-          maxAge: 7 * 24 * 60 * 60 * 1000, 
+          sameSite: "none",
+          maxAge: 7 * 24 * 60 * 60 * 1000,
         })
         .status(200)
         .json(rest);
@@ -76,9 +80,10 @@ async function google(req, res, next) {
         Math.random().toString(36).slice(-8) +
         Math.random().toString(36).slice(-8);
       const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+
       const newUser = new User({
         firstName:
-          req.body.name.split(' ').join('').toLowerCase() +
+          req.body.name.split(" ").join("").toLowerCase() +
           Math.random().toString(36).slice(-4),
         email: req.body.email.toLowerCase(),
         password: hashedPassword,
@@ -88,11 +93,11 @@ async function google(req, res, next) {
       const token = jwt.sign({ id: newUser._id }, JWT_SECRET);
       const { password: pass, ...rest } = newUser._doc;
       res
-        .cookie('authToken', token, {
+        .cookie("authToken", token, {
           httpOnly: true,
           secure: true,
-          sameSite: 'none',
-          maxAge: 7 * 24 * 60 * 60 * 1000, 
+          sameSite: "none",
+          maxAge: 7 * 24 * 60 * 60 * 1000,
         })
         .status(200)
         .json(rest);
@@ -101,11 +106,10 @@ async function google(req, res, next) {
     console.log("Error is here", error);
     next(error);
   }
-};
-
+}
 
 module.exports = {
   login,
   logout,
-  google
+  google,
 };
