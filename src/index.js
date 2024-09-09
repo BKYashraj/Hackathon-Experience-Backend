2475d870488ba726ac4e576b0acc1d36c88e6a00
Yashraj Pravin Desale <yashrajdesale1@gmail.com>
2024-09-09 14:37:07 +0530

    Added Periodic requesting mechanism to not stop the backend service

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
-const express = require('express')
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
-const ServerConfig = require('./config/serverConfig')
-const connectDB = require('./config/dbConfig')
-const User = require('./schema/userSchema')
-const userRouter = require('./routes/userRoute')
-const authRouter = require('./routes/authRoute')
-const { isLoggedIn } = require('./validation/authValidator')
-const uploader = require('./middlewares/multerMiddleware')
-const hackathonRouter = require('./routes/hackathonRoute')
-const paperRouter = require('./routes/paperRoute')
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const ServerConfig = require('./config/serverConfig');
+const connectDB = require('./config/dbConfig');
+const User = require('./schema/userSchema');
+const userRouter = require('./routes/userRoute');
+const authRouter = require('./routes/authRoute');
+const { isLoggedIn } = require('./validation/authValidator');
+const uploader = require('./middlewares/multerMiddleware');
+const hackathonRouter = require('./routes/hackathonRoute');
+const paperRouter = require('./routes/paperRoute');
 const cors = require('cors');
+const axios = require('axios');  // Add axios for making HTTP requests
 
-const app = express()
-
-
-// origin: 
+const app = express();
 
 app.use(cors({
   origin: ServerConfig.ORIGIN_LINK,
@@ -24,32 +22,37 @@ app.use(cors({
   credentials: true,
 }));
 
-// If request is in JSON, text, urlencoded it correctly reads by Express Server
-// deserialiser
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());// to access cookie by server when any service call server
+app.use(cookieParser());
 
-// Routing middleware
 app.use('/users', userRouter);
 app.use('/auth', authRouter);
 app.use('/hackathon', hackathonRouter);
 app.use('/paper', paperRouter);
 
-app.post('/ping', isLoggedIn,(req, res) => {
+app.post('/ping', isLoggedIn, (req, res) => {
   console.log(req.body);
   console.log('Auth Token:', req.cookies);
-  return res.json({message: 'pong'});
-})
+  return res.json({ message: 'pong' });
+});
 
-app.post('/papers', uploader.single('incomingFile'),(req, res) => {
-  return res.json({message: 'ok'});
+app.post('/papers', uploader.single('incomingFile'), (req, res) => {
+  return res.json({ message: 'ok' });
 });
 
 app.listen(ServerConfig.PORT, async () => {
   await connectDB();
-  console.log(`Example app listening on port ${ServerConfig.PORT}...`)
+  console.log(`Example app listening on port ${ServerConfig.PORT}...`);
+
+  // Auto-request to keep the server alive
+  setInterval(() => {
+    axios.post(`http://localhost:${ServerConfig.PORT}/ping`, {})
+      .then(() => console.log('Auto-ping sent to keep the server alive.'))
+      .catch(error => console.error('Error in auto-ping:', error.message));
+  }, 10 * 60 * 1000);  // Every 10 minutes
+});
 
   // For checking
   // const newUser = await User.create({
@@ -61,6 +64,4 @@ app.listen(ServerConfig.PORT, async () => {
   // });
   // console.log(newUser);
 
-
-})
 
